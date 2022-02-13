ce2f92a40b951621ccd2e7406bb80d01934ed254
bobshih <bobshih@hotmail.com>
2022-02-13 17:30:21 +0800

    upate run.py to read commands and on_member_remove on_member_join

--- run.py
+++ run.py
@@ -3,12 +3,17 @@ import yaml
 
 #導入 Discord.py
 import discord
-intents = discord.Intents(messages=True, guilds=True, members=True)
-#client 是我們與 Discord 連結的橋樑
-client = discord.Client(intents=intents)
 
+#client 是我們與 Discord 連結的橋樑
+# 控制 bot 有哪些權限，同時必須在
+intents = discord.Intents(messages=True, guilds=True, members=True)
+client = discord.Client(intents=intents)
 from src import Utils
-from src.Bot import OnInit
+from src.Bot import Bot
+from src.Bot.Entity import Member
+
+# a bot
+bot = Bot()
 # 載入 setting
 settings = Utils.Load_Setting()
 
@@ -17,21 +22,32 @@ settings = Utils.Load_Setting()
 #當機器人完成啟動時
 async def on_ready():
     print('目前登入身份：', client.user)
-    print(dir(client))
-    print(client.guilds)
     for guild in client.guilds:
-        guild_data = OnInit.GetGuildData(guild)
-        with open(os.path.join("data", str(guild.id) + '.yaml'), 'w', encoding='utf8') as fp:
-            yaml.dump(guild_data, fp, allow_unicode=True)
+        bot.InitGuildData(guild)
 
 @client.event
 #當有訊息時
-async def on_message(message):
+async def on_message(message: discord.Message):
     #排除自己的訊息，避免陷入無限循環
     if message.author == client.user:
         return
+    # 如果訊息由 += 開頭，傳入機器人內部
+    if message.content.startswith("+="):
+        await bot.ProcessMessage(message)
     #如果包含 ping，機器人回傳 pong
     if message.content == 'ping':
         await message.channel.send('pong')
+
+@client.event
+async def on_member_remove(member: discord.Member):
+    print("[Debug] in on_member_remove")
+    leave_member = Member.Init_wMember(member)
+    await bot.SendLeaveMSG(member.guild, leave_member)
+
+@client.event
+async def on_member_join(member: discord.Member):
+    print("[Debug] in on_member_join")
+    leave_member = Member.Init_wMember(member)
+    await bot.SendWelcomeMSG(member.guild, leave_member)
 
 client.run(settings.dc_bot_token) #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
