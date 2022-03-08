0452c2ce87fb0f4977e49addb73f77841ef88cc0
bobshih <bobshih@hotmail.com>
2022-03-09 00:26:07 +0800

    加入 channel 到 guilds 中，每個 guild 可以決定要追蹤那些頻道

--- src/Bot/Entity/Guild.py
+++ src/Bot/Entity/Guild.py
@@ -6,7 +6,7 @@ from discord import Guild as dGuild
 
 from src.Utils.bot_utils import SaveGuildData
 
-from . import Member
+from . import Member, ChannelData
 
 def _init_guild_setting(guild_setting: dict={}, discord_guild: dGuild = None)->dict:
     ''' 初始化 guild setting '''
@@ -27,6 +27,19 @@ def _init_guild_setting(guild_setting: dict={}, discord_guild: dGuild = None)->d
         guild_setting['setting']['leave_channel'] = -1
     if 'leave_text' not in guild_setting:
         guild_setting['setting']['leave_text'] = "default leave message: {user}"
+    # channel-related properties
+    if 'channel' not in guild_setting:
+        guild_setting['channel'] = {}
+    if 'channel_list' not in guild_setting['channel']:
+        guild_setting['channel']['channel_list'] = []
+    if 'notify_text_channel' not in guild_setting['channel']:      # 通知直播的文字頻道
+        guild_setting['channel']['notify_text_channel'] = -1
+    if 'waiting_msg' not in guild_setting['channel']:              # 待機通知
+        guild_setting['channel']['waiting_msg'] = ''
+    if 'start_stream_msg' not in guild_setting['channel']:         # 開始直播通知
+        guild_setting['channel']['start_stream_msg'] = ''
+    if 'end_stream_msg' not in guild_setting['channel']:           # 結束直播通知
+        guild_setting['channel']['end_stream_msg'] = ''
     return guild_setting
 
 class Guild:
@@ -39,11 +52,19 @@ class Guild:
         # guild info
         self.name = guild_setting['info']['name']
         self.id = guild_setting['info']['id']
-        # settings
+        # described channels
+        self.described_channels: List[ChannelData] = []
+        for channel_setting in guild_setting['channel']['channel_list']:
+            self.described_channels.append(ChannelData(channel_setting))
+        # message settings
         self.welcome_text: str = guild_setting['setting']['welcome_text']
         self.welcome_channel: int = guild_setting['setting']['welcome_channel']
         self.leave_text: str = guild_setting['setting']['leave_text']
         self.leave_channel: int = guild_setting['setting']['leave_channel']
+        self.notify_text_channel: int = guild_setting['channel']['notify_text_channel']
+        self.waiting_msg: str = guild_setting['channel']['waiting_msg']
+        self.start_stream_msg: str = guild_setting['channel']['start_stream_msg']
+        self.end_stream_msg: str = guild_setting['channel']['end_stream_msg']
     
     def UpdateGuildFile(self)->None:
         ''' 更新 Guild File '''
@@ -66,9 +87,22 @@ class Guild:
         self.leave_text = ' '.join(leave_txt)
         self.UpdateGuildFile()
 
+    def GetStartNotifyMSG(self, channel_id: str)->str:
+        ''' 取得 開始直播 通知 '''
+        for channel in self.described_channels:
+            if channel.id == channel_id:
+                msg = self.start_stream_msg.format()
+
     def GetSetting(self)->dict:
         ''' 取出 setting dict '''
         result = {
+            'channel': {
+                'channel_list': [ele.GetSetting() for ele in self.described_channels],
+                'notify_text_channel': self.notify_text_channel,
+                'waiting_msg': self.waiting_msg,
+                "start_stream_msg": self.start_stream_msg,
+                "end_stream_msg": self.end_stream_msg,
+            },
             'setting':{
                 'welcome_text': self.welcome_text,
                 'welcome_channel': self.welcome_channel,
