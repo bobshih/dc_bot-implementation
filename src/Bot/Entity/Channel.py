671c3f88221307faa0cd9acc1448d59b914bb545
bobshih <bobshih@hotmail.com>
2022-03-10 22:34:31 +0800

    modify the settings of channel data

--- src/Bot/Entity/Channel.py
+++ src/Bot/Entity/Channel.py
@@ -7,18 +7,21 @@ def _init_channel_setting(setting):
     assert 'id' in setting, '[Error] 沒有設定 channel ID 阿!!!'
     if 'name' not in setting:
         setting['name'] = ''
-    if 'desc' not in setting:
-        setting['desc'] = ''
     if 'last_stream_id' not in setting:
         setting['last_stream_id'] = ''
+    if 'text_channel' not in setting:
+        setting['text_channel'] = ''
+    if 'thread_id' not in setting:
+        setting['thread_id'] = ''
 
 class ChannelData:
     def __init__(self, setting: dict):
         _init_channel_setting(setting)
         self.id = setting['id']
         self.name = setting['name']
-        self.description = setting['desc']
         self.last_stream_id = setting['last_stream_id']     # 最後追蹤到的直播 id
+        self.text_channel = setting['text_channel']
+        self.thread_id = setting['thread_id']
         # initialize other default values
         self.live = False
         self.stream_id = ''         # 正在 stream 的 id
@@ -27,7 +30,8 @@ class ChannelData:
         setting = {
             "id": self.id,
             "name": self.name,
-            "desc": self.description,
-            "last_stram_id": self.last_stream_id
+            "last_stram_id": self.last_stream_id,
+            "text_channel": self.text_channel,
+            "thread_id": self.thread_id,
         }
         return setting
