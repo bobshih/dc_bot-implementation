cf3fe6726f1eae2c45c5a7f920b246ce2d0de865
bobshih <bobshih@hotmail.com>
2022-03-10 22:31:34 +0800

    task finished

--- src/Bot/DC_Bot.py
+++ src/Bot/DC_Bot.py
@@ -8,29 +8,29 @@ from discord import (
     Client
 )
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
 from typing import Dict, List, Tuple
 
-from .Entity import Guild, Member, ChannelData
+from .Entity import Guild_cls, Member
 from ..Utils import bot_utils
 
 BotPrefix = '+='
 
 class Bot:
-    def __init__(self, client_obj: Client):
+    def __init__(self, client_obj: Client, google_api_key: str):
         guild_settings = bot_utils.LoadGuildData()
+        self.google_api_key = google_api_key
         self.guilds = {
-            gid: Guild(setting) for gid, setting in guild_settings.items()
+            gid: Guild_cls(setting) for gid, setting in guild_settings.items()
         }
-        channel_settings = bot_utils.LoadChannelData()
         self.client = client_obj
-        # self.live_list = []             # 目前有在直播的 stream id (video id)
 
     def InitGuildData(self, guild: dGuild)->None:
         ''' 載入 guild 資料並更新 '''
         guild_id = guild.id
         if guild_id not in self.guilds:
-            self.guilds[guild_id] = Guild.Init_wGuild(guild)
+            self.guilds[guild_id] = Guild_cls.Init_wGuild(guild)
             bot_utils.SaveGuildData(guild_id, self.guilds[guild_id].GetSetting())
         else:
             print(f"[Warning] guild, {guild.name}, has been added in data.")
@@ -63,14 +63,18 @@ class Bot:
         else:
             print(f"[Error] guild, {guild.name}, has no channel whose id is {target_channel}")
     
-    def CheckLiveStreaming(self)->Dict[str, List[Tuple(str, str)]]:
-        ''' 檢查有沒有 live，此函式不管是不是已經停止直播了，專心檢查是否有直播 '''
-        result = defaultdict(list)
+    def CheckLiveStreaming(self)->None:
+        '''
+            檢查有沒有 live 要開始或正在直播，如果已經在直播了，就不管
+            此函式會影響後續檢查 live status 的 task
+            要把 Live 資訊放到 channel.stream_id
+        '''
         for guild_id, guild in self.guilds.items():
-            if guild.described_channels == []: continue
             if guild.notify_text_channel == '': continue
             for channel_data in guild.described_channels:
                 if channel_data.live:       # 已經在 live 了，不用檢查了
+                    continue
+                if channel_data.stream_id:  # 已經知道下一步 live stream id 了，不用再檢查
                     continue
                 page = requests.get(f"https://youtube.com/channel/{channel_data.id}/live")
                 page_soup = BeautifulSoup(page.text, 'html.parser')
@@ -79,27 +83,61 @@ class Bot:
                 href_html = link.get('href')
                 if 'watch?v=' in href_html:
                     live_streaming_id = href_html.split('watch?v=')[1]
-                    # self.live_list.append(live_streaming_id)
+                    channel_data.stream_id = live_streaming_id         # 在此更新 stream id
+        #             result[guild_id].append((channel_data.id, live_streaming_id))
+        # return dict(result)
+
+    def CheckStreamStatus(self)->Dict[str, List[Tuple[str, str]]]:
+        '''
+            檢查 stream_id 的直播狀態，根據狀態決定輸出的是 waiting msg 還是 live stream
+            必須前一步 CheckLiveStreaming 有處理到
+        '''
+        result = defaultdict(list)
+        for guild_id, guild in self.guilds.items():
+            if guild.notify_text_channel == '': continue
+            for channel_data in guild.described_channels:
+                if channel_data.stream_id == '':                # 沒有 stream id 表示沒有待機室或是直播
+                    continue
+                yt_api_url = "https://www.googleapis.com/youtube/v3/videos?id={}&part=snippet,liveStreamingDetails&key={}".format(
+                    channel_data.stream_id, self.google_api_key
+                )
+                res = requests.get(yt_api_url)
+                stream_data = res.json()['items'][0]
+                live_info = bot_utils.GetLiveStreamInfo(stream_data, channel_data.stream_id)
+                if live_info.live_status == 'upcoming':
+                    channel_data.live = False
+                    # 檢查是否超過 7 天，如果超過七天就不通知
+                    if (live_info.scheduled_start_time - datetime.now()).days >= 7:
+                        continue
+                    message = self.guilds[guild_id].GetWaitingMSG(live_info)
+                    result[guild_id].append((channel_data.text_channel, message))
+                elif live_info.live_status == 'live' and not channel_data.live:
+                    # 切換成 live 模式，送出 live string
                     channel_data.live = True
-                    result[guild_id].append((channel_data.id, live_streaming_id))
+                    message = self.guilds[guild_id].GetStartNotifyMSG(live_info)
+                    result[guild_id].append((channel_data.text_channel, message))
+                elif live_info.end_time != None:        # 已經有結束時間，表示直播結束了
+                    # 更新 channel data 去激活 CheckLiveStreaming 的工作
+                    channel_data.last_stream_id = channel_data.stream_id
+                    channel_data.stream_id = ''
+                    channel_data.live = False
+                    # 取得 end msg
+                    message = self.guilds[guild_id].GetEndMSG(live_info)
+                    result[guild_id].append((channel_data.text_channel, message))
         return dict(result)
 
     async def DoTasks(self)->None:
         ''' 機器人做事，會傳訊息出去 '''
         # 1. Check the streams which is starting
-        start_streams = self.CheckLiveStreaming()
+        self.CheckLiveStreaming()
         # 2. check waiting room
-        new_waiting = self.CheckWaitingRoom()
-        # 3. check steam ending
-        end_stream = self.CheckEndingStream()
-        # 4. send messages
-        # 4.1 send live message
-        for guild_id, live_infos in start_streams.items():
-            channel_id = self.guilds[guild_id].notify_text_channel
-            notify_channel = await self.client.fetch_channel(channel_id)
-            for channel_id, video_id in live_infos:
-                message = self.guilds[guild_id]
-                await notify_channel.send(message)
+        guild_messages = self.CheckStreamStatus()
+        # 3. send messages to each guild
+        for _, messages in guild_messages.items():
+            # channel_id = self.guilds[guild_id].notify_text_channel
+            for channel_id, msg in messages:
+                notify_channel = await self.client.fetch_channel(channel_id)
+                await notify_channel.send(msg)
 
     async def ProcessMessage(self, message: dMessage)->None:
         ''' process the message '''
