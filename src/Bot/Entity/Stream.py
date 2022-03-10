7a6b0673f31b7aa5ec38f2286ba60ff3db46ac91
bobshih <bobshih@hotmail.com>
2022-03-10 22:22:01 +0800

    add stream link in stream info

--- src/Bot/Entity/Stream.py
+++ src/Bot/Entity/Stream.py
@@ -6,6 +6,7 @@ class StreamInfo:
     def __init__(
         self,
         title,
+        link,
         live_status,            # 直播狀態: upcoming, live, none
         description,
         scheduled_start_time,
@@ -16,3 +17,4 @@ class StreamInfo:
         self.live_status = live_status
         self.scheduled_start_time = scheduled_start_time
         self.end_time = actual_end_time
+        self.link = link
