bf466ca9172d54522bc16e9b1b6663725d058c5b
bobshih <bobshih@hotmail.com>
2022-03-14 22:29:28 +0800

    split the process to two scripts

--- clock.py
+++ clock.py
@@ -1,19 +1,12 @@
 import os
 import urllib.request
-from flask import Flask
 
-from apscheduler.schedulers.background import BackgroundScheduler
-
-app = Flask(__name__)
-
-@app.route("/hello_world")
-def hello_world():
-    return "<p>Hello, World!</p>"
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 heroku_link = os.environ['heroku_link']
 # heroku_link = "http://127.0.0.1:5000/hello_world"
 
-sched = BackgroundScheduler()
+sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=3)#, minutes = 19)
 def scheduled_job():
