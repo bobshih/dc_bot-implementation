193622f301ed2bed901a2df5bcb5a2d28437bde0
bobshih <bobshih@hotmail.com>
2022-03-14 22:17:12 +0800

    add a flask app and fix problems about urllib

--- clock.py
+++ clock.py
@@ -1,16 +1,24 @@
 import os
-import urllib
+import urllib.request
+from flask import Flask
 
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = Flask(__name__)
+
+@app.route("/hello_world")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 heroku_link = os.environ['heroku_link']
+# heroku_link = "http://127.0.0.1:5000/hello_world"
 
-sched = BlockingScheduler()
+sched = BackgroundScheduler()
 
-@sched.scheduled_job('interval', minutes = 19)
+@sched.scheduled_job('interval', seconds=3)#, minutes = 19)
 def scheduled_job():
     conn = urllib.request.urlopen(heroku_link)
-        
+    print(conn.read())
     for key, value in conn.getheaders():
         print(key, value)
 
