5c01c321994f1b0d7aa18b4df9b7543ab0f73fea
bobshih <bobshih@hotmail.com>
2022-04-14 14:28:54 +0800

    remove command

--- run.py
+++ run.py
@@ -83,8 +83,6 @@ if __name__ == '__main__':
         #如果包含 ping，機器人回傳 pong
         if message.content == 'ping':
             await message.channel.send('pong')
-        if message.content == 'ping 光年 routine':
-            await message.channel.send('<@221661791262867456>光年生日快樂')
 
     @client.event
     async def on_member_remove(member: discord.Member):
