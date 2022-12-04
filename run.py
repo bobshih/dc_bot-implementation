bac68b82d8e707a1c6d71c1999d936ee462e6f77
bobshih <bobshih@hotmail.com>
2022-12-04 21:49:41 +0800

    移除掉 heroku link 的部分，不使用 heroku 了

--- run.py
+++ run.py
@@ -2,22 +2,6 @@
 import os
 import urllib.request
 
-from apscheduler.schedulers.background import BackgroundScheduler
-
-heroku_link = os.environ['heroku_link']
-# heroku_link = "http://127.0.0.1:5000/hello_world/local"
-
-sched = BackgroundScheduler()
-
-# @sched.scheduled_job('interval', minutes = 19)
-@sched.scheduled_job('interval', minutes = 14)
-def scheduled_job():
-    conn = urllib.request.urlopen(heroku_link)
-    print(conn.read())
-    for key, value in conn.getheaders():
-        print(key, value)
-sched.start()
-
 # web service part
 # from flask import Flask
 
